af5dfef9eeafd0872222b30a6f1b8d8d9d1fe7b5
Eessa1 <sabireessa@gmail.com>
2025-09-01 01:27:34 +0100

    + Added win conditions - I should probably optimise it the current system is just brute force

--- Naughts and crosses.py
+++ Naughts and crosses.py
@@ -1,23 +1,40 @@
 def boardsetup():
- board = [
+ boardSet = [
  [" ", " ", " "],
  [" ", " ", " "],
  [" ", " ", " "]
  ]
- for row in board:
+ for row in boardSet:
   print(" | ".join(row))
- return board
+ return boardSet
+
 def Game():
- board = boardsetup()
- inputx, inputy = GetUserInput()
- board[inputx][inputy] = "X"
- for row in board:
-  print(" | ".join(row))
- 
+ global GameOver
+ while GameOver == False:
+  global board
+  if board == []:
+    board = boardsetup()
+  inputx, inputy = GetUserInput()
+  board[inputy-1][inputx-1] = "X"
+  for row in board:
+    print(" | ".join(row))
+  CheckIfWin()
+  
+def CheckIfWin():
+ global board
+ global GameOver
+ x = 0
+ y = 0
+ if board[y][x] and board[y][x+1] and board[y][x+2] == 'X':
+  GameOver = True
  
 def GetUserInput():
- XValue = int(input("Enter the X value 0-2 inclusive"))
- YValue = int(input("Enter the Y value 0-2 inclusive"))
+ YValue = int(input("Enter the row 1-3 "))
+ XValue = int(input("Enter the column 1-3 "))
  return XValue,YValue
 
+GameOver= False
+board = []
+PlayerTurn = True
+AiTurn = False
 Game()
