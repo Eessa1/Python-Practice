18960e7b02e95fb53a6885af3d06d38a7c253361
Eessa1 <sabireessa@gmail.com>
2025-09-05 17:52:06 +0100

    Working on implementing minimax

--- Naughts and crosses.py
+++ Naughts and crosses.py
@@ -11,6 +11,7 @@ def boardsetup():
  return boardSet
 
 def Game():
+ global Result
  global PlayersTurn
  global GameOver
  while GameOver == False:
@@ -26,52 +27,85 @@ def Game():
   for row in board:
     print(" | ".join(row))
   CheckIfWin()
+ GameState()
+ print(Result)
   
 def CheckIfWin():
+ global Result
  global board
  global GameOver
  x = 0
  y = 0
  if board[y][x] == 'X' and board[y][x+1] == 'X' and board[y][x+2] == 'X':
   GameOver = True
+  Result = "Player"
  if board[y][x] == 'O' and board[y][x+1] == 'O' and board[y][x+2] == 'O':
   GameOver = True
+  Result = "Ai"
 
  if board[y][x] == 'X' and board[y+1][x] == 'X' and board[y+2][x] == 'X':
   GameOver = True
+  Result = "Player"
  if board[y][x] == 'O' and board[y+1][x] == 'O' and board[y+2][x] == 'O':
   GameOver = True
+  Result = "Ai"
 
  if board[y+1][x] == 'X' and board[y+1][x+1] == 'X' and board[y+1][x+2] == 'X':
   GameOver = True
+  Result = "Player"
  if board[y+1][x] == 'O' and board[y+1][x+1] == 'O' and board[y+1][x+2] == 'O':
   GameOver = True
+  Result = "Ai"
 
  if board[y+2][x] == 'X' and board[y+2][x+1] == 'X' and board[y+2][x+2] == 'X':
   GameOver = True
+  Result = "Player"
  if board[y+2][x] == 'O' and board[y+2][x+1] == 'O' and board[y+2][x+2] == 'O':
   GameOver = True
+  Result = "Ai"
 
  if board[y][x+1] == 'X' and board[y+1][x+1] == 'X' and board[y+2][x+1] == 'X':
   GameOver = True
+  Result = "Player"
  if board[y][x+1] == 'O' and board[y+1][x+1] == 'O' and board[y+2][x+1] == 'O':
   GameOver = True
+  Result = "Ai"
 
  if board[y][x+2] == 'X' and board[y+1][x+2] == 'X' and board[y+2][x+2] == 'X':
   GameOver = True
+  Result = "Player"
  if board[y][x+2] == 'O' and board[y+1][x+2] == 'O' and board[y+2][x+2] == 'O':
   GameOver = True
+  Result = "Ai"
 
  if board[y][x] == 'X' and board[y+1][x+1] == 'X' and board[y+2][x+2] == 'X':
   GameOver = True
+  Result = "Player"
  if board[y][x] == 'O' and board[y+1][x+1] == 'O' and board[y+2][x+2] == 'O':
   GameOver = True
+  Result = "Ai"
 
  if board[y][x+2] == 'X' and board[y+1][x+1] == 'X' and board[y+2][x] == 'X':
   GameOver = True
+  Result = "Player"
  if board[y][x+2] == 'O' and board[y+1][x+1] == 'O' and board[y+2][x] == 'O':
   GameOver = True
+  Result = "Ai"
  
+def GameState():
+ global Result
+ global board
+ global GameOver
+ counter = 0
+ for rows in board:
+  for cell in rows:
+   if cell == " ":
+    counter += 1
+ if counter == 0 and GameOver == False:
+   GameOver = True
+   Result = "Draw"
+
+
 def AiTurn():
  global board
  global PlayersTurn
@@ -82,6 +116,7 @@ def AiTurn():
   AiX = random.randint(0,2)
  PlayersTurn = True 
  return AiX,AiY
+
 def GetUserInput():
  global PlayersTurn
  YValue = int(input("Enter the row 1-3 "))
@@ -93,6 +128,7 @@ PlayersTurn = True
 GameOver= False
 board = []
 PlayerTurn = True
+Result = " "
 Game()
 
 
