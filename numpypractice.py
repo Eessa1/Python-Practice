17026eed94a671b384bf9862f2c119d17d25cfa5
Eessa1 <sabireessa@gmail.com>
2026-06-13 20:24:42 +0100

    matplot practice

--- numpypractice.py
+++ numpypractice.py
@@ -1,5 +1,13 @@
 import numpy as np
-
-a = np.array([[1,2],[3,4]])
-b = np.array([[5,6],[7,8]])
-print(np.dot(a,b))
+import matplotlib.pyplot as plt
+x = [1, 2, 3, 4, 5]
+y = [1, 4, 9, 16, 25]
+x2 = [1,2,3,4,5]
+y2 = [1,8,27,64,125]
+plt.plot(x,y)
+plt.plot(x2,y2)
+plt.title("Y = X squared and x cubed")
+plt.legend(["X squared","X cubed"], loc = "lower right")
+plt.xlabel("X axis")
+plt.ylabel("Y axis")
+plt.show()
