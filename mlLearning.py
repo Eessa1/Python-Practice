1d0da4c0bf910379b8d446fb5594de5156e04d17
Eessa1 <sabireessa@gmail.com>
2026-06-22 18:10:27 +0100

    understanding rmse

--- mlLearning.py
+++ mlLearning.py
@@ -1,3 +1,24 @@
-print("ml ")
-print("changed")
+import numpy as np
+import matplotlib.pyplot as plt
+
+#  house data
+sizes =  np.array([50, 60, 70, 80, 90, 100])
+prices = np.array([140, 195, 200, 260, 280, 310])
+
+# plot 
+plt.scatter(sizes, prices)
+plt.xlabel("Size (sqm)")
+plt.ylabel("Price (£k)")
+plt.title("House Prices")
+plt.show()
+m = 3
+c = 10
+predicted = m* sizes + c
+errors = predicted - prices
+rmse = np.sqrt(np.mean(errors**2))
+print ("RMSE:", rmse)
+plt.scatter(sizes,prices, label = "real")
+plt.plot(sizes, predicted, color = 'red', label="prediction")
+plt.legend()
+plt.show()
 
