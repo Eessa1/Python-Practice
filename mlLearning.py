e838e284efda94229d33368ff1d35be598f12a0c
Eessa1 <sabireessa@gmail.com>
2026-08-09 18:38:32 +0100

    hi

--- mlLearning.py
+++ mlLearning.py
@@ -3,6 +3,7 @@ import pandas as pd
 import tarfile
 import urllib.request
 import matplotlib.pyplot as plt
+import numpy as np
 
 def load_housing_data():
     tarball_path = Path("datasets/housing.tgz")
@@ -15,12 +16,5 @@ def load_housing_data():
     return pd.read_csv(Path("datasets/housing/housing.csv"))
 
 housing_full = load_housing_data()
-plt.rc('font', size=14)
-plt.rc('axes', labelsize=14, titlesize=14)
-plt.rc('legend', fontsize=14)
-plt.rc('xtick', labelsize=10)
-plt.rc('ytick', labelsize=10)
-
-housing_full.hist(bins=50, figsize=(12, 8))
 
 plt.show()
