7f2a26ba3e2d6805925160bd5ea39f7d0b71cf96
Eessa1 <sabireessa@gmail.com>
2026-08-11 21:35:59 +0100

    trying to understand this book

--- mlLearning.py
+++ mlLearning.py
@@ -17,4 +17,9 @@ def load_housing_data():
 
 housing_full = load_housing_data()
 
-plt.show()
+def shuffle_and_split(data,test_ratio, rng):
+    shuffled_indices = rng.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
