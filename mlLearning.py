80263682a831e6ce24e5684d0a4dd03951f80777
Eessa1 <sabireessa@gmail.com>
2026-09-01 23:35:38 +0100

    hi

--- mlLearning.py
+++ mlLearning.py
@@ -48,4 +48,6 @@ age_simil_35= rbf_kernel(housing[["housing_median_age"]],[[35]],gamma=0.1)
 scaled_labels = target_scaler.fit_transform(housing_labels.to_frame())
 model = LinearRegression
 model.fit(housing[["median_income"]], scaled_labels)
-
+some_new_data = housing[["median_income"]].iloc[:5]
+scaled_predictions = model.predict(some_new_data)
+predictions = target_scaler.inverse_transform(scaled_predictions)
