b91d805c56d4443136b8fee6331872ef5c87d74f
Eessa1 <sabireessa@gmail.com>
2026-09-11 20:23:52 +0100

    ready

--- mlLearning.py
+++ mlLearning.py
@@ -23,21 +23,37 @@ class ClusterSimilarity(BaseEstimator,TransformerMixin):
         self.gamma = gamma
         self.random_state = random_state
     def fit(self,X, y= None, sample_weight = None):
-        self.kmeans = KMeans(self.n_clusters,random_state= self.random_state)
+        self.kmeans_ = KMeans(self.n_clusters,random_state= self.random_state)
         self.kmeans_.fit(X, sample_weight= sample_weight)
         return self
     def transform(self,X):
         return rbf_kernel(X,self.kmeans_.cluster_centers_,gamma=self.gamma)
-    def get_feature_names_out(self,names = None):
+    def get_feature_names_out(self,names= None):
         return [f"Cluster {i} similarity" for i in range(self.n_clusters)]
+def column_ratio(X):
+    return X[:,[0]] / X[:,[1]]
+def ratio_name(function_transformer, feature_names_in):
+    return ["ratio"]
+def ratio_pipeline():
+    return make_pipeline(SimpleImputer(strategy= "median"),FunctionTransformer(column_ratio,feature_names_out=ratio_name),StandardScaler())
+log_pipeline = make_pipeline(SimpleImputer(strategy="median"),FunctionTransformer(np.log,feature_names_out="one-to-one"),StandardScaler())
 cat_pipeline = make_pipeline(SimpleImputer(strategy="most_frequent"),OneHotEncoder(handle_unknown="ignore"))
+cluster_simil = ClusterSimilarity(n_clusters= 10, gamma = 1, random_state=42)
+default_num_pipeline = make_pipeline(SimpleImputer(strategy="median"),StandardScaler())
+preprocessing = ColumnTransformer([("bedrooms", ratio_pipeline(), ["total_bedrooms","total_rooms"]), ("rooms_per_house", ratio_pipeline(), ["total_rooms","households"]),
+("people_per_house", ratio_pipeline(), ["population",
+"households"]),
+ ("log", log_pipeline, ["total_bedrooms", "total_rooms",
+"population",
+ "households", "median_income"]),
+ ("geo", cluster_simil, ["latitude", "longitude"]),
+ ("cat", cat_pipeline, make_column_selector(dtype_include=object)),],remainder=default_num_pipeline)
 log_transformer = FunctionTransformer(np.log,inverse_func=np.exp)
 target_scaler = StandardScaler()
 std_scaler = StandardScaler()
 cat_encoder = OneHotEncoder()
 imputer = SimpleImputer(strategy="median")
 num_pipeline = Pipeline([("impute", SimpleImputer(strategy="median")),("standardize", StandardScaler())])
-preprocessing = ColumnTransformer([("num",num_pipeline, make_column_selector(dtype_include=np.number)),("cat",cat_pipeline,make_column_selector(dtype_include=object))])
 def load_housing_data():
     tarball_path = Path("datasets/housing.tgz")
     if not tarball_path.is_file():
@@ -68,7 +84,5 @@ model.fit(housing[["median_income"]],housing_labels)
 data = housing[["median_income"]].iloc[:5]
 predictions = model.predict(data)
 log_pop = log_transformer.transform(housing[["population"]])
-housing_prepared = preprocessing.fit_transform(housing)
-df_hp = pd.DataFrame(housing_prepared, columns = preprocessing.get_feature_names_out(), index= housing.index)
-cluster_simil = ClusterSimilarity(n_clusters= 10, gamma = 1, random_state=42)
 similarities = cluster_simil.fit_transform(housing[["latitude","longitude"]],sample_weight=housing_labels)
+
